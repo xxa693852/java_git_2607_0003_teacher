6004e773a58a445af3b505e2d704db7d211c2e15
edward_dev1 <leonardo071123@gmail.com>
2026-07-16 22:06:00 +0800

    update: 修改dev-App.java#2
    (cherry picked from commit 2a8ec79b323f797b766aab9920350559c28555f3)

--- src/App.java
+++ src/App.java
@@ -5,6 +5,8 @@ public class App {
         System.out.println("main-update-#1-#2-#3");
 
         System.out.println("admin-update-#1-#2");
+
+        System.out.println("dev-update-#1-#2");
         
     }
 }
